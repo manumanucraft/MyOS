3bb55b2598d147bc32668e9052921d0948bda12a
manumanucraft <manumanucraft@gmail.com>
2015-05-01 10:52:03 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,24 +1,15 @@
+local blacklist = {"installer.lua","README.md","LICENSE",".gitignore","doc"}
+
 if not term.isColor() then return end
 term.clear()
-term.setCursorPos(20,7)
-term.write("Windows 10")
-function white(x,y)
-  paintutils.drawPixel(x,y,colors.white)
-end
-function black(x,y)
-  paintutils.drawPixel(x,y,colors.black)
-end
+term.setCursorPos(5,5)
+term.write("MyOS installing path: ")
+path = read()
+os.remove(path.."/*")
+term.clear()
+term.setCursorPos(1,1)
+shell.run("pastebin", "run", "VuBNx3va", "-u", "manumanucraft", "-r", "MyOS", path)
 
-white(25,9)
-white(23,11)
-white(27,11)
-white(22,13)
-white(28,13)
-white(23,15)
-white(27,15)
-sleep(.1)
-white(25,17)
-black(27,15)
-sleep(.1)
-white(27,15)
-black(28,13)
+for k,v in ipairs(blacklist) do
+  os.remove(path.."/"..v)
+end
